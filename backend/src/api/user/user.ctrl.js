a0f828d81613c47c1dd6aca7a341f2138e0439f1
s0698s <33773381+s0698s@users.noreply.github.com>
2017-11-21 23:02:28 +0900

    Update user.ctrl.js

--- backend/src/api/user/user.ctrl.js
+++ backend/src/api/user/user.ctrl.js
@@ -18,9 +18,24 @@ exports.view = (req, res) => {
     });
 };
 
-// 시용자 정보 수정 (비밀번호 변경, (디스플레이이름변경?), 프로필사진변경, 프로필내용변경)
+// 사용자 정보 수정 (닉네임변경, 프로필사진변경, 프로필내용변경)
 exports.edit = (req, res) => {
+    if(!req.user) return res.status(403).json({msg: "로그인 중이 아닙니다."}); // 로그인 케이스 판별
 
+    const userName = req.params.user_name;
+    const { displayName, photo, profile } = req.body; // 변경 정보
+
+    User.findByUserName(userName).then(user => {
+            if (req.user.id != user.id) return res.status(403).json({msg : "토큰 발행 당시 id와 현재 요청 id 일치 하지 않음"}); 
+            if (!user) return res.status(403).json({msg: "해당 userName의 유저가 존재하지 않습니다."});
+
+            user.edit({displayName, photo, profile}).then(updated => {
+                const { id, displayName, photo, profile } = updated;
+                const result = { id, displayName, photo, profile };
+                res.json({msg: "Update OK", result});
+            });
+        }).catch(error => {
+            return res.status(403).json({msg: "Promise 에러"});
+        });
 };
-
 // 프로필사진같은거 업로드 api 따로 구현해야되나? (이건 json 형식이 아니라 form-data 형식일듯..)
