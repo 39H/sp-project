4369c824321ad85ab15d2023bc15d57199f65048
s0698s <33773381+s0698s@users.noreply.github.com>
2017-11-23 00:51:02 +0900

    Update index.js

--- backend/src/api/list/index.js
+++ backend/src/api/list/index.js
@@ -2,4 +2,7 @@ const express = require('express');
 const list = express.Router();
 const listCtrl = require('./list.ctrl');
 
+list.get('/recent', listCtrl.listByRecent);
+list.get('/likes', listCtrl.listByLikes);
+
 module.exports = list;
