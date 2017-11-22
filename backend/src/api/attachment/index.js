8d86d1e2433baec85457c8208be706651f51f4f0
s0698s <33773381+s0698s@users.noreply.github.com>
2017-11-23 00:01:46 +0900

    Update index.js

--- backend/src/api/attachment/index.js
+++ backend/src/api/attachment/index.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const attachment = express.Router();
 const attachmentCtrl = require('./attachment.ctrl');
+const multer = require('multer');
 
+const upload = multer({
+    storage: multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+      },
+      filename: function (req, file, cb) {
+        cb(null, file.originalname);
+      }
+    }),
+  });
+
+attachment.get('/:work_id', attachmentCtrl.get); // 작품의 첨부 파일들 조회
+attachment.post('/:work_id', upload.single('img'), attachmentCtrl.upload); // 작품에 파일 첨부
 module.exports = attachment;
