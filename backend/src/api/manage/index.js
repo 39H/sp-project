2bd14a2f12ce2f55f70daaa63c60e7790b1deb89
s0698s <33773381+s0698s@users.noreply.github.com>
2017-11-30 01:29:03 +0900

    Update index.js

--- backend/src/api/manage/index.js
+++ backend/src/api/manage/index.js
@@ -13,4 +13,10 @@ manage.get('/thread/mine', manageCtrl.getMyThreads);
 // 내가 쓴 댓글 목록
 manage.get('/comment/mine', manageCtrl.getMyComments);
 
+//프로필이미지 업로드
+manage.post('/profile/:user_id', manageCtrl.profileupload.single('img'), manageCtrl.profile);
+
+//섬네일이미지업로드
+manage.post('/thumbnail/:work_id', manageCtrl.thumbnailupload.single('img'), manageCtrl.thumbnail);
+
 module.exports = manage;
